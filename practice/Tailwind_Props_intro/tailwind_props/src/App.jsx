277b12cfc894ce276f3f403bc6c1419144ec420d
siddhexh <siddhesh.iiitp@gmail.com>
2026-09-09 23:50:06 +0530

    prop functionality

--- practice/Tailwind_Props_intro/tailwind_props/src/App.jsx
+++ practice/Tailwind_Props_intro/tailwind_props/src/App.jsx
@@ -1,9 +1,10 @@
 import Card from "./components/card";
 
 function App() {
+  console.log(Card)
   return (
     <div className="min-h-screen bg-gray-950 flex items-center justify-center">
-      <Card name="Siddhesh" />
+      <Card name="Siddhesh Umbarkar" />
     </div>
   );
 }
